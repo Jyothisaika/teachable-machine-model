d308b853ae5dabf0d2a44ff8de2d640ee86ce294
Jyothisaika <96777274+Jyothisaika@users.noreply.github.com>
2022-06-23 17:34:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,32 @@
     });
     
  }
+ console.log("ML5 version", ml5.version);
+
+ classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/HuxXH6DYy/model.json", model_loaded);
+
+
+ function model_loaded(){
+
+   console.log("My Teachable Model has been loaded");
+
+ }
+
+
+ function check(){
+
+   my_img = document.getElementById("snapshot");
+
+   classifier.classify(my_img, gotResults);
+ }
+
+ function gotResults(error, results){
+
+   if(error){
+      console.error(error);
+   }
+   else {
+      console.log(results);
+   }
+   
+ }
